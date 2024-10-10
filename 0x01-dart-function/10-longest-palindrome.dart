88e3d0d690cfd6a005d1d7195e4516f523735454
Christopher Diggs <65854363+DiggDiggs@users.noreply.github.com>
2024-10-10 05:11:59 +0000

    longest

---
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -0,0 +1,34 @@
+bool isPalindrome(String s) {
+  if (s.length < 3) {
+    return false;
+  }
+  String reversed = s.split('').reversed.join('');
+  return s == reversed;
+}
+
+String longestPalindrome(String s) {
+  if (s.length < 3) {
+    return "none";
+  }
+
+  String longestPal = "none";
+
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 3; j <= s.length; j++) {
+      String substring = s.substring(i, j);
+      if (isPalindrome(substring) && substring.length >= 3) {
+        if (substring.length > longestPal.length) {
+          longestPal = substring;
+        }
+      }
+    }
+  }
+
+  return longestPal;
+}
+
+void main() {
+  print(longestPalindrome("abcde")); // Output: none
+  print(longestPalindrome("azeghjhg")); // Output: ghjhg
+  print(longestPalindrome("aaabvf")); // Output: aaa
+}
