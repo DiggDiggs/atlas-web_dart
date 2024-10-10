33793c9fc80b5655467c0256106eae16578c4cda
Christopher Diggs <65854363+DiggDiggs@users.noreply.github.com>
2024-10-10 06:10:56 +0000

    inheritance

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,75 +1,43 @@
-class Password {
-  String _password;
+import '6-password.dart';
 
-  Password({required String password}) : _password = password;
-
-  bool isValid() {
-    return _password.length >= 8 &&
-        _password.length <= 16 &&
-        _password.contains(RegExp(r'[A-Z]')) &&
-        _password.contains(RegExp(r'[a-z]')) &&
-        _password.contains(RegExp(r'[0-9]'));
-  }
-
-  @override
-  String toString() {
-    return 'Your Password is: $_password';
-  }
-
-  // Getter for password
-  String get password => _password;
-
-  // Setter for password
-  set password(String newPassword) {
-    _password = newPassword;
-  }
-}
-
-class User {
+class User extends Password {
   int id;
   String name;
   int age;
   double height;
-  Password _userPassword;
+  String user_password;
 
   User({
     required this.id,
     required this.name,
     required this.age,
     required this.height,
-    required String user_password,
-  }) : _userPassword = Password(password: user_password);
+    required this.user_password,
+  }) : super(password: user_password);
 
-  Map<String, dynamic> toJson() {
-    return {
+  Map toJson() {
+    return ({
       'id': id,
       'name': name,
       'age': age,
       'height': height,
-      'user_password': _userPassword.password,
-    };
+    });
   }
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
+    return (User(
       id: userJson['id'],
       name: userJson['name'],
       age: userJson['age'],
       height: userJson['height'],
-      user_password: userJson['user_password'],
-    );
+      user_password: userJson['user_password'] ?? '',
+    ));
   }
 
   @override
   String toString() {
-    return 'User(id : $id ,name: $name, age: $age, height: $height, Password: ${_userPassword.isValid()})';
-  }
+    final newPassword = new Password(password: this.user_password);
 
-  // Getter for user_password
-  String get user_password => _userPassword.password;
-
-  // Setter for user_password
-  set user_password(String newPassword) {
-    _userPassword.password = newPassword;
+    return ('User(id : $id ,name: $name, age: $age, height: $height, Password: ${newPassword.isValid()})');
   }
 }
